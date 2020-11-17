6a507f627350d1562aee477f0226db42fb35bb9b
SteffenTunkel <steffen.tunkel@gmail.com>
2020-11-17 13:30:21 +0100

    setup spark java docker

--- build_docker.py
+++ build_docker.py
@@ -4,7 +4,7 @@ import os
 os.system('cmd /c "docker stop $(docker ps -a -q)"')
 
 # run atoml docker build
-os.system('cmd /c "docker build -t atoml_docker atoml_docker"')
+#os.system('cmd /c "docker build -t atoml_docker atoml_docker"')
 
 # run sklearn docker build
 #os.system('cmd /c "docker build -t sklearn_docker sklearn_docker"')
@@ -13,10 +13,11 @@ os.system('cmd /c "docker build -t atoml_docker atoml_docker"')
 #os.system('cmd /c "docker build -t weka_docker weka_docker"')
 
 # run spark docker build
-#os.system('cmd /c "docker build -t spark_docker spark_docker"')
+os.system('cmd /c "docker build -t spark_docker spark_docker"')
+
 
 # run sklearn docker build for atoml tests
-os.system('cmd /c "docker build -t sklearn_atoml_docker sklearn_atoml_docker"')
+#os.system('cmd /c "docker build -t sklearn_atoml_docker sklearn_atoml_docker"')
 
 
 
--- run_docker.py
+++ run_docker.py
@@ -3,7 +3,7 @@ import os
 # cmd /k -> remain ___ cmd /c -> terminate
 
 # run atoml test generation
-os.system('cmd /c "docker run --mount type=bind,source=%cd%/generated-tests,target=/generated-tests --mount type=bind,source=%cd%/algorithm-descriptions,target=/testdata atoml_docker"')
+#os.system('cmd /c "docker run --mount type=bind,source=%cd%/generated-tests,target=/generated-tests --mount type=bind,source=%cd%/algorithm-descriptions,target=/testdata atoml_docker"')
 
 # run sklearn classification
 #os.system('cmd /c "docker run --mount type=bind,source=%cd%/predictions,target=/log --mount type=bind,source=%cd%/dataset,target=/data sklearn_docker"')
@@ -17,5 +17,8 @@ os.system('cmd /c "docker run --mount type=bind,source=%cd%/generated-tests,targ
 # run sklearn connected to atoml v1
 #os.system('cmd /c "docker run --mount type=bind,source=%cd%/generated-tests/sklearn/smokedata,target=/smokedata --mount type=bind,source=%cd%/generated-tests/sklearn/morphdata,target=/morphdata --mount type=bind,source=%cd%/dataset,target=/data --mount type=bind,source=%cd%/predictions,target=/log sklearn_atoml_docker"')
 
+os.system('cmd /c"docker run spark_docker"')
+
+
 # run sklearn connected to atoml v2
-os.system('cmd /c "docker run --mount type=bind,source=%cd%/generated-tests/sklearn,target=/sklearn --mount type=bind,source=%cd%/predictions,target=/log sklearn_atoml_docker"')
+#os.system('cmd /c "docker run --mount type=bind,source=%cd%/generated-tests/sklearn,target=/sklearn --mount type=bind,source=%cd%/predictions,target=/log sklearn_atoml_docker"')
