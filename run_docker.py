c51ecaf691beaaccf3bed97ec0cfa235c444bcdd
SteffenTunkel <steffen.tunkel@gmail.com>
2020-11-19 22:40:42 +0100

    add weka test prediction csv saving; update readme

--- run_docker.py
+++ run_docker.py
@@ -5,6 +5,10 @@ import os
 #os.system('cmd /c "docker run --mount type=bind,source=%cd%/algorithm-descriptions,target=/testdata atoml_docker"')
 
 def runMyDocker(name, *bindings):
+	# This function starts a docker container
+	# The parameter name includes the container image name, which needs to be build before
+	# The parameter bindings is a list of string couples. It contains the mount bindings with
+	#	the source relative to the current dictionary as the first string and the target as the second
     cmdstr = 'cmd /c"docker run '
     for bind in bindings:
         cmdstr += '--mount type=bind,source=%cd%'
@@ -19,13 +23,12 @@ def runMyDocker(name, *bindings):
 atomlMounts = [["/generated-tests", "/generated-tests"],["/algorithm-descriptions", "/testdata"]]
 runMyDocker("atoml_docker", *atomlMounts)
 
-sparkMounts = [["/generated-tests/spark/src", "/code/src"], ["/predictions/spark", "/code/log"]]
-runMyDocker("spark_docker", *sparkMounts)
+sklearnMounts = [["/generated-tests/sklearn" , "/sklearn"], ["/predictions/sklearn", "/log"]]
+#runMyDocker("sklearn_docker", *sklearnMounts)
 
-wekaMounts = [["/generated-tests/weka/src", "/code/src"], ["/predictions/weka", "/code/log"]]
+wekaMounts = [["/generated-tests/weka/src", "/code/src"], ["/predictions/weka", "/log"]]
 runMyDocker("weka_docker", *wekaMounts)
 
-sklearnMounts = [["/generated-tests/sklearn" , "/sklearn"], ["/predictions/sklearn", "/log"]]
-runMyDocker("sklearn_docker", *sklearnMounts)
-
+sparkMounts = [["/generated-tests/spark/src", "/code/src"], ["/predictions/spark", "/code/log"]]
+#runMyDocker("spark_docker", *sparkMounts)
 
