2cdfd9337a73adf351959f42088d7ca47fc92bd1
SteffenTunkel <steffen.tunkel@stud.uni-goettingen.de>
2020-11-24 17:12:35 +0100

    change compare function

--- compare_results.py
+++ compare_results.py
@@ -1,29 +1,51 @@
+# at the moment limited to the Uniform data
+
 import os
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 #from sklearn.preprocessing import LabelEncoder
 
-df = pd.read_csv('algorithm-descriptions/framework_matching.csv')
-print(df)
+
+def createConfusionMatrix(algorithm_identifier, predictions, frameworks, df, iteration):
+	print('%d: Comparation between %s and %s for %s' % (iteration, frameworks[0].upper(), frameworks[1].upper(), algorithm_identifier))
+	cm = confusion_matrix(predictions[0], predictions[1])
+	print(cm)
+	if cm[0,1] == 0 and cm[1,0] == 0 and ( cm[0,0] != 0 or cm[1,1] != 0):
+		same = True
+	else:
+		same = False
+	frame_data = [[same, cm[0,0], cm[0,1], cm[1,0], cm[1,1], ('%s_%s_%s'%(frameworks[0],frameworks[1],algorithm_identifier))]]
+	df = df.append(pd.DataFrame(data=frame_data, columns=df.columns), ignore_index=True)
+	return df, same, cm
 
 
-for index, row in df.iterrows():
-	algorith_ident = row['sklearn']
+print('MatchingTable:')
+
+matching_table = pd.read_csv('algorithm-descriptions/framework_matching.csv')
+print(matching_table)
+
+result_columns = ["same","TP","FP","TN","FN","compared_algorithm"]
+results_df = pd.DataFrame(columns=result_columns)
+print(results_df)
+for index, row in matching_table.iterrows():
+	algorithm_ident = row['sklearn']
 
 	print('\nThe predictions in the following files are compared with each other:')
 
+	# get sklearn csv file
 	sklearn_csvfile = 'predictions/sklearn/pred_SKLEARN_' + row['sklearn'] + '_Uniform.csv'
 	print(sklearn_csvfile)
 	pred_sklearn = pd.read_csv(sklearn_csvfile, header=None)
 
+	# get weka csv file
 	weka_csvfile = 'predictions/weka/pred_WEKA_' + row['weka'] + '_Uniform.csv'
 	print(weka_csvfile)
 	pred_weka = pd.read_csv(weka_csvfile)
 	pred_weka = pred_weka["prediction"]
 	
+	# spark CSVs are currently saved in a weird format (csv file is in a named folder)
 	spark_path= 'predictions/spark/pred_SPARK_' + row['spark'] + '_Uniform'
 	spark_csv_folder = [f for f in os.listdir(spark_path) if os.path.isfile(os.path.join(spark_path, f))]
-	#print(spark_csv_folder)
 	for file in spark_csv_folder:
 		if file.endswith(".csv"):
 			print(spark_path + '/' + file)
@@ -45,11 +67,12 @@ for index, row in df.iterrows():
 	#le = LabelEncoder()
 	#pred_sklearn = le.fit_transform(pred_sklearn)
 
+	results_df, _, _=createConfusionMatrix(algorithm_ident, [pred_sklearn, pred_spark], ["sklearn", "spark"], results_df, index)
+	results_df, _, _=createConfusionMatrix(algorithm_ident, [pred_sklearn, pred_weka], ["sklearn", "weka"], results_df, index)
+	results_df, _, _=createConfusionMatrix(algorithm_ident, [pred_spark, pred_weka], ["spark", "weka"], results_df, index)
 
-	# calculate and print the confusion matix
-	print('Comparation between SKLEARN and SPARK:', index, algorith_ident)
-	print(confusion_matrix(pred_sklearn, pred_spark))
+print("\n ResultDataFrame:")
+print(results_df)
+results_df.to_csv('algorithm-descriptions/framework_matching_results.csv', index=False)
 
-	# calculate and print the confusion matix
-	print('Comparation between SKLEARN and WEKA:', index, algorith_ident)
-	print(confusion_matrix(pred_sklearn, pred_weka))
+
