ab4420017e9c12bebb113fabb2ef5d578fb7feef
SteffenTunkel <steffen.tunkel@stud.uni-goettingen.de>
2021-03-04 19:04:53 +0100

    minor changes

--- atoml_docker/run_atoml_testgeneration.sh
+++ atoml_docker/run_atoml_testgeneration.sh
@@ -3,5 +3,5 @@
 for file in testdata/*.yml
 do
 echo create tests from "$file":
-java -jar build/libs/atoml-0.1.0.jar -f "$file" -nomorph -n "$1" -predictions -t 1000
+java -jar build/libs/atoml-0.1.0.jar -f "$file" -nomorph -n "$1" -predictions
 done
