3f3dae61f9fb65acbfdb00a5365bc54ebf0998d1
SteffenTunkel <steffen.tunkel@gmail.com>
2021-03-15 17:16:23 +0100

    change system test

--- tests/system_tests/test_run_tool.py
+++ tests/system_tests/test_run_tool.py
@@ -6,8 +6,8 @@ from atoml_cmp.external_data_utils import create_wine_arff, create_breast_cancer
 class TestRunThrough(unittest.TestCase):
     def test_atoml_cmp_usecase(self):
         num_logreg_csv = 4 * 8
-        create_wine_arff()
-        create_breast_cancer_arff()
+        # create_wine_arff() # currently deactivated because overwriting leads to permission denied error on linux
+        # create_breast_cancer_arff()
         csv_files_evaluated = main("tests/system_tests/resources/test_dockerlist.json",
                                    yaml_folder="tests/system_tests/resources/test_algorithm_descriptions")
         self.assertEqual(csv_files_evaluated, num_logreg_csv)
