333a7ad396eaf130f2fba4f7ff45d4d595ef586e
SteffenTunkel <steffen.tunkel@gmail.com>
2021-03-22 23:07:51 +0100

    restructure system test

---
+++ tests/system_tests/test_resources/test_atoml_docker/TestCatalogSelection.java
@@ -0,0 +1,51 @@
+package atoml.testgen;
+
+import java.util.List;
+
+import atoml.metamorphic.MetamorphicTest;
+
+import atoml.smoke.Bias;
+import atoml.smoke.Categorical;
+import atoml.smoke.DisjointCategorical;
+import atoml.smoke.DisjointNumeric;
+import atoml.smoke.LeftSkew;
+import atoml.smoke.ManyCategories;
+import atoml.smoke.MaxDouble;
+import atoml.smoke.MaxFloat;
+import atoml.smoke.MinDouble;
+import atoml.smoke.MinFloat;
+import atoml.smoke.OneClass;
+import atoml.smoke.Outlier;
+import atoml.smoke.RandomCategorial;
+import atoml.smoke.RandomNumeric;
+import atoml.smoke.RightSkew;
+import atoml.smoke.SmokeTest;
+import atoml.smoke.Split;
+import atoml.smoke.StarvedBinary;
+import atoml.smoke.StarvedMany;
+import atoml.smoke.Uniform;
+import atoml.smoke.VeryLarge;
+import atoml.smoke.VerySmall;
+import atoml.smoke.Zeroes;
+import atoml.smoke.SmoketestFromArff;
+
+public class TestCatalog {
+
+	/**
+	 * List of all smoke tests
+	 * Used by atoml_cmp for the test case generation
+	 * Alongside the implemented datasets (see imports) also external can be used with:
+	 *      new SmoketestFromArff("TestName", "/ArffFile.arff")
+	 * In this case the data in the given arff file ist used training and testing.
+	 * To use separated training and test sets use:
+	 *      new SmoketestFromArff("TestName", "/ArffFileTraining.arff", "/ArffFileTest.arff")
+	 */
+	public static final List<SmokeTest> SMOKETESTS = List.of(
+			new Uniform(),
+			new RandomNumeric(),
+	        new SmoketestFromArff("BreastCancer", "/BreastCancer_training.arff", "/BreastCancer_test.arff"));
+	/**
+	 * Immutable list of all metamorphic tests, needed by atoml but not used by atoml_cmp.
+	 */
+	public static final List<MetamorphicTest> METAMORPHICTESTS = List.of();
+}
--- tests/system_tests/test_run_tool.py
+++ tests/system_tests/test_run_tool.py
@@ -1,16 +1,18 @@
 import unittest
 from atoml_cmp.run_tool import main
-from atoml_cmp.external_data_utils import create_wine_arff, create_breast_cancer_arff
 
 
 class TestRunThrough(unittest.TestCase):
     def test_atoml_cmp_usecase(self):
-        num_logreg_csv = 4 * 8
-        # !!! currently deactivated because overwriting leads to permission denied error on linux
-        # create_wine_arff()
-        # create_breast_cancer_arff()
-        csv_files_evaluated = main("tests/system_tests/resources/test_dockerlist.json",
-                                   yaml_folder="tests/system_tests/resources/test_algorithm_descriptions")
-        # !!! currently deactivated because caret is not working properly on linux (maybe due to not overwriting)
-        # concerns: VerySmall, Outlier, Zeroes
-        # self.assertEqual(csv_files_evaluated, num_logreg_csv)
+        """Tests a possible run through of the overall tool (including docker build, docker run and evaluation).
+
+        Assert how many csv files the evaluation unit read.
+        In order to make sure that the test fails if the pipeline runs through empty."""
+        num_datasets = 3
+        num_algorithms = 1
+        num_frameworks = 4
+        expected_num_csv_files = num_frameworks * num_algorithms * num_datasets
+
+        csv_files_evaluated = main("tests/system_tests/test_resources/test_dockerlist.json",
+                                   yaml_folder="tests/system_tests/test_resources/test_algorithm_descriptions")
+        self.assertEqual(csv_files_evaluated, expected_num_csv_files)
