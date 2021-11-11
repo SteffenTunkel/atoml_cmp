b633abc040e14fb692e04cc0f62404a957f4cf89
SteffenTunkel <steffen.tunkel@gmail.com>
2021-11-11 12:55:56 +0100

    update function calls in tests

--- tests/unit_tests/test_evaluation.py
+++ tests/unit_tests/test_evaluation.py
@@ -7,14 +7,14 @@ class ConfusionMatrixPositiveTest(unittest.TestCase):
     def test_behavior_on_identical(self):
         input1 = pd.Series([1, 0, 1, 1, 0, 0, 1, 1])
         input2 = pd.Series([1, 0, 1, 1, 0, 0, 1, 1])
-        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix([input1, input2], False)
+        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix(input1, input2, False)
         self.assertEqual(equal_flag, True)
         np.testing.assert_equal(confusion_matrix, [[3, 0], [0, 5]])
 
     def test_behavior_on_different(self):
         input1 = pd.Series([1, 0, 1, 1, 0, 0, 1, 1])
         input2 = pd.Series([1, 0, 1, 1, 1, 0, 1, 1])
-        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix([input1, input2], False)
+        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix(input1, input2, False)
         self.assertEqual(equal_flag, False)
         tp, tn, fp, fn = 2, 5, 0, 1
         np.testing.assert_equal(confusion_matrix, [[tp, fn], [fp, tn]])
@@ -22,7 +22,7 @@ class ConfusionMatrixPositiveTest(unittest.TestCase):
     def test_all_zero(self):
         input1 = pd.Series([0, 0, 0, 0, 0, 0, 0, 0])
         input2 = pd.Series([0, 0, 0, 0, 0, 0, 0, 0])
-        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix([input1, input2], False)
+        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix(input1, input2, False)
         self.assertEqual(equal_flag, True)
         tp, tn, fp, fn = 8, 0, 0, 0
         np.testing.assert_equal(confusion_matrix, [[tp, fn], [fp, tn]])
@@ -30,7 +30,7 @@ class ConfusionMatrixPositiveTest(unittest.TestCase):
     def test_all_ones(self):
         input1 = pd.Series([1, 1, 1, 1, 1, 1, 1, 1])
         input2 = pd.Series([1, 1, 1, 1, 1, 1, 1, 1])
-        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix([input1, input2], False)
+        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix(input1, input2, False)
         self.assertEqual(equal_flag, True)
         tp, tn, fp, fn = 0, 8, 0, 0
         np.testing.assert_equal(confusion_matrix, [[tp, fn], [fp, tn]])
@@ -38,7 +38,7 @@ class ConfusionMatrixPositiveTest(unittest.TestCase):
     def test_all_different_mixed(self):
         input1 = pd.Series([1, 0, 1, 1, 0, 1, 0, 1])
         input2 = pd.Series([0, 1, 0, 0, 1, 0, 1, 0])
-        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix([input1, input2], False)
+        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix(input1, input2, False)
         self.assertEqual(equal_flag, False)
         tp, tn, fp, fn = 0, 0, 5, 3
         np.testing.assert_equal(confusion_matrix, [[tp, fn], [fp, tn]])
@@ -46,7 +46,7 @@ class ConfusionMatrixPositiveTest(unittest.TestCase):
     def test_all_different_same(self):
         input1 = pd.Series([1, 1, 1, 1, 1, 1, 1, 1])
         input2 = pd.Series([0, 0, 0, 0, 0, 0, 0, 0])
-        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix([input1, input2], False)
+        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix(input1, input2, False)
         self.assertEqual(equal_flag, False)
         tp, tn, fp, fn = 0, 0, 8, 0
         np.testing.assert_equal(confusion_matrix, [[tp, fn], [fp, tn]])
@@ -55,6 +55,6 @@ class ConfusionMatrixPositiveTest(unittest.TestCase):
         input1 = pd.Series([1, 0, 1, 1, 0, 0, 1, 1])
         input2 = pd.Series([1, 0, 1, 1, 0, 0, 1, 1])
         print("\n### Test print output for 'create_confusion_matrix':")
-        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix([input1, input2], True)
+        equal_flag, confusion_matrix = atoml_cmp.evaluation.create_confusion_matrix(input1, input2, True)
         self.assertEqual(equal_flag, True)
         np.testing.assert_equal(confusion_matrix, [[3, 0], [0, 5]])
